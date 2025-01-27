7d1e64b88f3ea3278ef25f0da498ffc43a40be9c
Viktor Kucher <viktorkucer49@gmail.com>
2025-01-27 12:40:54 +0200

    Change task 1

--- src/com/shpp/rshmelev/cs/Assignment1Part1.java
+++ src/com/shpp/rshmelev/cs/Assignment1Part1.java
@@ -8,23 +8,25 @@ public class Assignment1Part1 extends SuperKarel {
         pickBeeper(); // Pick up a beeper from the current location
         returnOnStartPosition(); // Return Karel to its starting position
     }
-    // Method to move Karel to the newspaper's position
-    public void runToNewspaper() throws Exception {
-        turnRight(); // Turn Karel 90 degrees to the right
-        move(); // Move Karel forward one step
-        turnLeft(); // Turn Karel 90 degrees to the left
+    // Move Karel forward four step
+    private void moveFourPositions() throws Exception{
         move(); // Move Karel forward one step
         move(); // Move Karel forward one step
         move(); // Move Karel forward one step
         move(); // Move Karel forward one step
     }
+
+    // Method to move Karel to the newspaper's position
+    public void runToNewspaper() throws Exception {
+        turnRight(); // Turn Karel 90 degrees to the right
+        move(); // Move Karel forward one step
+        turnLeft(); // Turn Karel 90 degrees to the left
+        moveFourPositions();
+    }
     // Method to return Karel to its starting position
     public void returnOnStartPosition() throws Exception{
         turnAround(); // Turn Karel 180 degrees to face the opposite direction
-        move(); // Move Karel forward one step
-        move(); // Move Karel forward one step
-        move(); // Move Karel forward one step
-        move(); // Move Karel forward one step
+        moveFourPositions();
         turnRight(); // Turn Karel 90 degrees to the right
         move(); // Move Karel forward one step
     }
